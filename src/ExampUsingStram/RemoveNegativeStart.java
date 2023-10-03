84ae3d5cbe68d6ca48f4fbfb6cfac52092abad6f
Mizanurmd <mizanur021991@gmail.com>
2023-10-03 11:13:03 +0600

    Stream API Related

---
+++ src/ExampUsingStram/RemoveNegativeStart.java
@@ -0,0 +1,18 @@
+package ExampUsingStram;
+
+import java.util.Arrays;
+
+
+public class RemoveNegativeStart {
+	public static void main(String[]args)
+	{
+		
+		int[]ar = {9,-13,-12,-10,5,-6,-5,-7,2,-3,-100,20,15};
+		Arrays.sort(ar);
+		System.out.println(Arrays.toString(ar));
+		
+	
+		
+		
+	}
+}
